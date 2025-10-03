7b95da9fb7408fb6a0b4fcb247f2f15ace7bf759
Pritam Rauniyar <pritamrauniyar.np@gmail.com>
2025-10-03 20:04:18 +0530

    design change multiple consecutive operator fixed

--- script.js
+++ script.js
@@ -2,26 +2,50 @@ let showResult = document.getElementById("res-text");
 buttons = document.querySelectorAll("button");
 let res = "";
 let temp = "";
+
+function animateButton(button) {
+  button.classList.add("pressed");
+  setTimeout(() => {
+    button.classList.remove("pressed");
+  }, 150);
+}
+
+function isOperator(char) {
+  return ['+', '-', '*', '/', '%'].includes(char);
+}
+
 for (item of buttons) {
   item.addEventListener("click", (event) => {
-    button = event.target.innerText;
-    if (button == "x") {
-      button = "*";
-      temp = res;
-      temp += "x";
-      res += button;
-      showResult.value = temp;
-    } else if (button == "C") {
+    const button = event.target;
+    animateButton(button);
+    let buttonText = button.innerText;
+    if (buttonText == "x") {
+      buttonText = "*";
+    }
+    if (buttonText == "C") {
       res = "";
       temp = "";
       showResult.value = temp;
-    } else if (button == "=") {
-      res = eval(res);
-      temp = res;
-      showResult.value = res;
+    } else if (buttonText == "=") {
+      try {
+        res = eval(res);
+        temp = res;
+        showResult.value = res;
+      } catch (e) {
+        showResult.value = "Error";
+        res = "";
+        temp = "";
+      }
     } else {
-      res += button;
-      temp += button;
+      // Prevent multiple consecutive operators
+      if (isOperator(buttonText) && res.length > 0 && isOperator(res.slice(-1))) {
+        // Replace last operator with new one
+        res = res.slice(0, -1) + buttonText;
+        temp = temp.slice(0, -1) + buttonText;
+      } else {
+        res += buttonText;
+        temp += buttonText;
+      }
       showResult.value = temp;
     }
   });
